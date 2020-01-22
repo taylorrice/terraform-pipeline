1c6f595efe04be58d305c7503e775ab366632a6a
Taylor Rice <taylor.rice@rearc.io>
2020-01-22 14:45:47 -0500

    add vars

---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "private_subnet_a_id"{
+
+}
+
+variable "private_subnet_b_id"{
+
+}
+
+variable "region"{
+
+}
+
+variable "aws_account_id" {
+
+}
+
+variable "environment" {
+  default = "terraform"
+}
+
+variable "github_token" {
+
+}
+
+variable "repo_owner" {
+
+}
+
+variable "app_name" {
+
+}
+
+variable "branch_name" {
+
+}
